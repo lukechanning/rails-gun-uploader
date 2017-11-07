146c2c4f783f77c2659caf9f7a1251d0599a22bb
Luke Patrick <luke@snodandjeff.com>
2017-11-07 06:36:11 +0000

    Spec out the uploader, prep for completed controller code

--- spec/features/uploads/uploader_spec.rb
+++ spec/features/uploads/uploader_spec.rb
@@ -1,12 +1,11 @@
 require 'rails_helper'
 
 feature 'User uploads a new item' do
-    scenario "they click to the upload page" do
+    scenario 'they click to the upload page' do
         visit uploads_new_path
-        fill_in 'file_name', with: 'README Test'
-        attach_file('uploads__card-form-file', Rails.root + 'README.md')
+        fill_in 'file_name', with: 'Image Test'
+        attach_file('file', Rails.root + 'public/logo.png')
         click_button 'Punch It, Chewie'
-        expect(page).to have_content 'Success'
+        expect(page).to have_content 'Upload was saved to disk'
     end
-    
 end
